856411725dc6dcd2b2a817f1b57f0a70bef9fbe9
BlauGea1C <blaugeacots@cev.com>
2025-03-19 13:30:11 +0100

    mapa_03

--- Assets/Scrips/MAPA/Scr_CanvasMapa.cs
+++ Assets/Scrips/MAPA/Scr_CanvasMapa.cs
@@ -7,24 +7,8 @@ public class Scr_CanvasMapa : MonoBehaviour
 {
     public void ChangeScene(int Scene)
     {
-        Debug.Log("Cambiando a la escena: " + Scene);
-        StartCoroutine(LoadSceneAsync(Scene)); // Usamos una carga asincrónica
+        SceneManager.LoadScene(Scene);
     }
 
-    private IEnumerator LoadSceneAsync(int sceneIndex)
-    {
-        // Aquí desactivamos la UI si es necesario
-        // Por ejemplo: myCanvas.SetActive(false);
-
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
-
-        // Espera a que la escena se haya cargado completamente
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-
-        // Aquí puedes volver a activar la UI si la desactivaste
-        // Por ejemplo: myCanvas.SetActive(true);
-    }
 }
+
