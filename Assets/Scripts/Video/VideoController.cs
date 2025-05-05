19165c90f5214ec5ec9758969224373d862a005a
EnricMari1C <blaugeacots@cev.com>
2025-05-05 14:15:29 +0200

    final_01

--- Assets/Scripts/Video/VideoController.cs
+++ Assets/Scripts/Video/VideoController.cs
@@ -83,18 +83,18 @@ public class VideoController : MonoBehaviour
     public VideoClip videoFin;     // video al terminar
     public VideoClip video1;
     public VideoClip video2;
-    public VideoClip video3;
-    public VideoClip video4;
+   // public VideoClip video3;
+   // public VideoClip video4;
 
     public Button button1;
-    public Button button2;
-    public Button button3;
-    public Button button4;
+   // public Button button2;
+  //  public Button button3;
+   // public Button button4;
 
     private bool played1 = false;
     private bool played2 = false;
-    private bool played3 = false;
-    private bool played4 = false;
+    //private bool played3 = false;
+  //  private bool played4 = false;
 
     private bool videoPlaying = false;
 
@@ -103,9 +103,9 @@ public class VideoController : MonoBehaviour
         videoCanvas.gameObject.SetActive(false);
 
         button1.onClick.AddListener(() => TryPlayVideo(video1, ref played1));
-        button2.onClick.AddListener(() => TryPlayVideo(video2, ref played2));
-        button3.onClick.AddListener(() => TryPlayVideo(video3, ref played3));
-        button4.onClick.AddListener(() => TryPlayVideo(video4, ref played4));
+        //button2.onClick.AddListener(() => TryPlayVideo(video2, ref played2));
+      //  button3.onClick.AddListener(() => TryPlayVideo(video3, ref played3));
+      //  button4.onClick.AddListener(() => TryPlayVideo(video4, ref played4));
 
         videoPlayer.loopPointReached += OnVideoEnd;
 
